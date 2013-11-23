0df40f6d30c9b32087963ac354286b679deb9e80
lastagile <lastdanc@gmail.com>
2013-11-23 17:07:51 +0800

    add csg

---
+++ others/csg.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+usage()
+{
+    echo "usage : csg.sh src_path <-r>"
+    echo "I will create file list in ~/gcscope/cscope.filek"
+    echo "src_path can be file or dir, and -r is for recursive in src_path"
+}
+if [ $# -lt 1 ];
+then
+    usage
+    exit
+fi
+
+CSCOPE_PATH=~/gcscope/cscope.file
+
+if [ ! -d ~/gcscope ]
+then
+    mkdir -p ~/gcscope
+fi
+
+touch $CSCOPE_PATH
+
+if [ -d $1 ];
+then
+    # add the dir to cscope.filek
+    cd $1
+    # Using SRC_PATH let find cmd get the absolute path, and maxdepth reduce recursive find
+    SRC_PATH=$PWD
+    if [ $# -eq 1 ];
+    then
+        find $SRC_PATH -maxdepth 1 -name "*.h" -o -name "*.c" -o -name "Makefile" -o -name "makefile" >> $CSCOPE_PATH
+    else
+        find $SRC_PATH -name "*.h" -o -name "*.c" -o -name "Makefile" -o -name "makefile" >> $CSCOPE_PATH
+    fi
+elif [ -f $1 ];
+then
+    # Add file
+    echo "${PWD}/$1" >> $CSCOPE_PATH
+fi
+
+# remove duplication
+awk '!x[$0]++' $CSCOPE_PATH > /tmp/cscope.tmp && rm $CSCOPE_PATH && mv /tmp/cscope.tmp $CSCOPE_PATH
